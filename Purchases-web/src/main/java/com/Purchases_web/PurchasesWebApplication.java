ced1c3f94c248bb981db5d0b6a47bea593ea8090
unknown <diana_00-138@hotmail.com>
2024-06-25 18:17:11 +0200

    init spring boot

---
+++ Purchases-web/src/main/java/com/Purchases_web/PurchasesWebApplication.java
@@ -0,0 +1,13 @@
+package com.Purchases_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchasesWebApplication.class, args);
+	}
+
+}
---
+++ Purchases-web/src/test/java/com/Purchases_web/PurchasesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Purchases_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
