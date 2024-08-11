9aa21cd9996ca3c4f104a3a48d243fa983ba2373
unknown <diana_00-138@hotmail.com>
2024-08-11 14:27:12 +0200

    DB origin

--- cartshipping/src/Shoppingcart.java
+++ cartshipping/src/Shoppingcart.java
@@ -1,3 +1,4 @@
+package cartshipping.src;
 public class Shoppingcart {
 
     String productName;
---
+++ src/Java/User.java
@@ -0,0 +1,16 @@
+package Java;
+/**
+ * User
+ */
+public class User {
+
+    String username;
+    String password;
+    
+    
+    public void getSession(){
+        
+    }
+
+    
+}
